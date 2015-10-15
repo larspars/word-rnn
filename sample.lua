945f1e6783311e160d2de2bc8b641cca23ed4572
larspars <larseidnes@gmail.com>
2015-10-15 21:39:29 +0200

    Added functionality for continously reading new seed text from stdin

--- sample.lua
+++ sample.lua
@@ -14,6 +14,7 @@ require 'nngraph'
 require 'optim'
 require 'lfs'
 
+require 'util.GloVeEmbedding'
 require 'util.OneHot'
 require 'util.misc'
 local CharSplitLMMinibatchLoader = require 'util.CharSplitLMMinibatchLoader'
@@ -34,6 +35,7 @@ cmd:option('-temperature',1,'temperature of sampling')
 cmd:option('-gpuid',0,'which gpu to use. -1 = use CPU')
 cmd:option('-verbose',1,'set to 0 to ONLY print the sampled text, no diagnostics')
 cmd:option('-skip_unk',0,'whether to skip UNK tokens when sampling')
+cmd:option('-input_loop',0,'whether to read new seed text from stdin after having finished sampling')
 cmd:option('-word_level',0,'whether to operate on the word level, instead of character level (0: use chars, 1: use words)') --todo: set this in checkpoint
 cmd:text()
 
@@ -77,77 +79,94 @@ for c,i in pairs(vocab) do ivocab[i] = c end
 
 -- initialize the rnn state to all zeros
 gprint('creating an LSTM...')
-local current_state
-local num_layers = checkpoint.opt.num_layers
-current_state = {}
-for L = 1,checkpoint.opt.num_layers do
-    -- c and h for all layers
-    local h_init = torch.zeros(1, checkpoint.opt.rnn_size)
-    if opt.gpuid >= 0 then h_init = h_init:cuda() end
-    table.insert(current_state, h_init:clone())
-    table.insert(current_state, h_init:clone())
-end
-state_size = #current_state
+
 
 -- do a few seeded timesteps
 local seed_text = opt.primetext
-if string.len(seed_text) > 0 then
-    gprint('seeding with ' .. seed_text)
-    gprint('--------------------------')
-    for c in CharSplitLMMinibatchLoader.tokens(seed_text, opt.word_level == 1) do --todo: word_level should be stored in checkpoint
-        prev_char = torch.Tensor{vocab[c]}
-        io.write(ivocab[prev_char[1]])
-        if opt.gpuid >= 0 then prev_char = prev_char:cuda() end
+
+repeat
+
+    local current_state
+    local num_layers = checkpoint.opt.num_layers
+    current_state = {}
+    for L = 1,checkpoint.opt.num_layers do
+        -- c and h for all layers
+        local h_init = torch.zeros(1, checkpoint.opt.rnn_size)
+        if opt.gpuid >= 0 then h_init = h_init:cuda() end
+        table.insert(current_state, h_init:clone())
+        table.insert(current_state, h_init:clone())
+    end
+    state_size = #current_state
+    if string.len(seed_text) > 0 then
+        gprint('seeding with ' .. seed_text)
+        gprint('--------------------------')
+        local tokens = {}
+        for c in CharSplitLMMinibatchLoader.tokens(seed_text, opt.word_level == 1) do
+            if vocab[c] == nil then c = c:lower() end
+            tokens[#tokens + 1] = c
+        end
+        --tokens[#tokens + 1] = '.'
+        for _, c in ipairs(tokens) do --todo: word_level should be stored in checkpoint
+            local idx = vocab[c]
+            if idx ~= nil then
+                prev_char = torch.Tensor{idx}
+                if opt.gpuid >= 0 then prev_char = prev_char:cuda() end
+                local lst = protos.rnn:forward{prev_char, unpack(current_state)}
+                -- lst is a list of [state1,state2,..stateN,output]. We want everything but last piece
+                current_state = {}
+                for i=1,state_size do table.insert(current_state, lst[i]) end
+                prediction = lst[#lst] -- last element holds the log probabilities
+            end
+        end
+    else
+        -- fill with uniform probabilities over characters (? hmm)
+        gprint('missing seed text, using uniform probability over first character')
+        gprint('--------------------------')
+        prediction = torch.Tensor(1, #ivocab):fill(1)/(#ivocab)
+        if opt.gpuid >= 0 then prediction = prediction:cuda() end
+    end
+
+    -- start sampling/argmaxing
+    for i=1, opt.length do
+
+        -- log probabilities from the previous timestep
+        if opt.sample == 0 then
+            -- use argmax
+            -- TODO: Skip UNK
+            local _, prev_char_ = prediction:max(2)
+            prev_char = prev_char_:resize(1)
+        else
+            -- use sampling
+            prediction:div(opt.temperature) -- scale by temperature
+            local probs = torch.exp(prediction):squeeze()
+            probs:div(torch.sum(probs)) -- renormalize so probs sum to one
+
+            if opt.skip_unk then
+                prev_char = torch.multinomial(probs:float(), 2):float()
+                prev_char = prev_char[1] == vocab["UNK"] and prev_char[{{2}}] or prev_char[{{1}}]
+            else
+                prev_char = torch.multinomial(probs:float(), 1):resize(1):float()
+            end
+        end
+
+        -- forward the rnn for next character
         local lst = protos.rnn:forward{prev_char, unpack(current_state)}
-        -- lst is a list of [state1,state2,..stateN,output]. We want everything but last piece
         current_state = {}
         for i=1,state_size do table.insert(current_state, lst[i]) end
         prediction = lst[#lst] -- last element holds the log probabilities
-    end
-else
-    -- fill with uniform probabilities over characters (? hmm)
-    gprint('missing seed text, using uniform probability over first character')
-    gprint('--------------------------')
-    prediction = torch.Tensor(1, #ivocab):fill(1)/(#ivocab)
-    if opt.gpuid >= 0 then prediction = prediction:cuda() end
-end
 
--- start sampling/argmaxing
-for i=1, opt.length do
-
-    -- log probabilities from the previous timestep
-    if opt.sample == 0 then
-        -- use argmax
-        local _, prev_char_ = prediction:max(2)
-        prev_char = prev_char_:resize(1)
-    else
-        -- use sampling
-        prediction:div(opt.temperature) -- scale by temperature
-        local probs = torch.exp(prediction):squeeze()
-        probs:div(torch.sum(probs)) -- renormalize so probs sum to one
-
-        if opt.skip_unk then
-            prev_char = torch.multinomial(probs:float(), 2):float()
-            prev_char = prev_char[1] == vocab["UNK"] and prev_char[{{2}}] or prev_char[{{1}}]
-        else
-            prev_char = torch.multinomial(probs:float(), 1):resize(1):float()
+        word = ivocab[prev_char[1]]
+        if opt.word_level and word == "RN" then 
+            word = "\n"
+        end
+        io.write(word)
+        if opt.word_level then
+            io.write(" ")
         end
     end
-
-    -- forward the rnn for next character
-    local lst = protos.rnn:forward{prev_char, unpack(current_state)}
-    current_state = {}
-    for i=1,state_size do table.insert(current_state, lst[i]) end
-    prediction = lst[#lst] -- last element holds the log probabilities
-
-    word = ivocab[prev_char[1]]
-    if opt.word_level and word == "RN" then 
-        word = "\n"
+    io.write('\n') io.flush()
+    if opt.input_loop == 1 then
+        seed_text = io.read()
     end
-    io.write(word)
-    if opt.word_level then
-        io.write(" ")
-    end
-end
-io.write('\n') io.flush()
+until opt.input_loop ~= 1
 
